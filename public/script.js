9666ad302ae3dfe6beb6eab6faaa5123e62c0174
Rami1125 <rami.msarwa1@gmail.com>
2025-05-31 11:11:00 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,7 @@
+function sendData() {
+  const data = { message: "שלום מגיטהאב!" };
+  google.script.run.withSuccessHandler(response => {
+    alert("נשלח: " + response);
+  }).doPost({ postData: JSON.stringify(data) });
+}
 
